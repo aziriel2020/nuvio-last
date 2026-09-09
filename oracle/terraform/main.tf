e2f70bd3c7485d6cfae22a5a19bdc83a0546b8f8
aziriel2020 <72296439+aziriel2020@users.noreply.github.com>
2026-09-09 11:05:59 +0200

    infra: use latest Ubuntu image compatible with Ampere A1

--- oracle/terraform/main.tf
+++ oracle/terraform/main.tf
@@ -26,7 +26,6 @@ data "oci_identity_availability_domains" "ads" {
 data "oci_core_images" "ubuntu_a1" {
   compartment_id           = var.compartment_ocid
   operating_system         = "Canonical Ubuntu"
-  operating_system_version = "24.04"
   shape                    = "VM.Standard.A1.Flex"
   sort_by                  = "TIMECREATED"
   sort_order               = "DESC"
