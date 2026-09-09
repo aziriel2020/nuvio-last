81de4a5d9cf0a259dacb3a49bd702588aaf4797e
aziriel2020 <72296439+aziriel2020@users.noreply.github.com>
2026-09-09 11:04:54 +0200

    infra: add Oracle Always Free Terraform stack

---
+++ oracle/terraform/variables.tf
@@ -0,0 +1,41 @@
+variable "tenancy_ocid" {
+  type      = string
+  sensitive = true
+}
+
+variable "user_ocid" {
+  type      = string
+  sensitive = true
+}
+
+variable "fingerprint" {
+  type      = string
+  sensitive = true
+}
+
+variable "private_key_path" {
+  type      = string
+  sensitive = true
+}
+
+variable "region" {
+  type = string
+}
+
+variable "compartment_ocid" {
+  type = string
+}
+
+variable "ssh_public_key" {
+  type = string
+}
+
+variable "instance_name" {
+  type    = string
+  default = "nuvio-production"
+}
+
+variable "ssh_ingress_cidr" {
+  type    = string
+  default = "0.0.0.0/0"
+}
